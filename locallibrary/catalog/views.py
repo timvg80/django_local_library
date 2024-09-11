e2b98ff7109e492c087f12964f578b2b219ac5f2
Tim Vivian-Griffiths <tim.vivian.griffiths@tutanota.com>
2024-09-11 06:13:45 +0100

    Adding basic num_visits functionality

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,11 +17,17 @@ def index(request):
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
 
+    # Number of visits to the site - recorded in the session
+    num_visits = request.session.get("num_visits", 0)
+    num_visits += 1
+    request.session["num_visits"] = num_visits
+
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template with the context
